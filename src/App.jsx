a6d445e6e8d4d655f96d426b2f751a173514903d
AYAN BHATTACHARJEE <bhattacharjeeayan93@gmail.com>
2025-07-05 03:13:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold">TODOS</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
